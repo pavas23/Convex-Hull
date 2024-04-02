0e96c81bea86682a4fe945005f00784fa918252a
Pavas Garg <pavasgarg@Pavass-MacBook-Pro.local>
2024-04-02 21:11:44 +0530

    updated doc

--- client/src/components/JarvisMarchCodeDoc.jsx
+++ client/src/components/JarvisMarchCodeDoc.jsx
@@ -37,7 +37,10 @@ function JarvisMarchCodeDocumentation() {
         <div className="slide">
           {slideIndex === 0 && (
             <div className="introPara">
-              <div className="imgContainerOuter" style={{alignItems:'flex-start'}}>
+              <div
+                className="imgContainerOuter"
+                style={{ alignItems: "flex-start" }}
+              >
                 <div className="imgContainerInner">
                   <img src={jm1_code} alt="JM1" style={{ maxWidth: "100%" }} />
                 </div>
@@ -51,68 +54,115 @@ function JarvisMarchCodeDocumentation() {
                     modi reiciendis at atque ducimus, voluptatem vero? Minus
                     voluptate nihil nemo totam nesciunt cum perspiciatis beatae
                     nostrum voluptates soluta necessitatibus consectetur ab
-                    aperiam ipsam optio temporibus, quidem vitae aliquam
-                    !
+                    aperiam ipsam optio temporibus, quidem vitae aliquam !
                   </p>
                 </div>
               </div>
             </div>
           )}
           {slideIndex === 1 && (
-             <div className="introPara">
-             <div className="imgContainerOuter" style={{alignItems:'flex-start'}}>
-               <div className="imgContainerInner">
-                 <img src={jm2_code} alt="JM1" style={{ maxWidth: "100%" }} />
-               </div>
-               <div className="textContainer">
-                 <p style={{ marginTop: "0" }}>
-                 <h3 style={{"marginTop":"0px"}}> Function: orientation(p, q, r)</h3>
-                  <p>
-                    This function determines the orientation of three points, denoted as <code>p</code>, <code>q</code>, and <code>r</code>, in a two-dimensional plane.
-                  </p>
-                  <h4>Parameters:</h4>
-                  <ul>
-                    <li><code>p</code>: The first point object having the coordinates <code>x</code> and <code>y</code> in a two-dimensional plane.</li>
-                    <li><code>q</code>: The second point object having the coordinates <code>x</code> and <code>y</code> in a two-dimensional plane.</li>
-                    <li><code>r</code>: The third point object having the coordinates <code>x</code> and <code>y</code> in a two-dimensional plane.</li>
-                  </ul>
-                  <h4>Return Value:</h4>
+            <div className="introPara">
+              <div
+                className="imgContainerOuter"
+                style={{ alignItems: "flex-start" }}
+              >
+                <div className="imgContainerInner">
+                  <img src={jm2_code} alt="JM1" style={{ maxWidth: "100%" }} />
+                </div>
+                <div className="textContainer">
+                  <p style={{ marginTop: "0" }}>
+                    <h3 style={{ marginTop: "0px" }}>
+                      {" "}
+                      Function: orientation(p, q, r)
+                    </h3>
+                    <p>
+                      This function determines the orientation of three points,
+                      denoted as <code>p</code>, <code>q</code>, and{" "}
+                      <code>r</code>, in a two-dimensional plane.
+                    </p>
+                    <h4>Parameters:</h4>
+                    <ul>
+                      <li>
+                        <code>p</code>: The first point object having the
+                        coordinates <code>x</code> and <code>y</code> in a
+                        two-dimensional plane.
+                      </li>
+                      <li>
+                        <code>q</code>: The second point object having the
+                        coordinates <code>x</code> and <code>y</code> in a
+                        two-dimensional plane.
+                      </li>
+                      <li>
+                        <code>r</code>: The third point object having the
+                        coordinates <code>x</code> and <code>y</code> in a
+                        two-dimensional plane.
+                      </li>
+                    </ul>
+                    <h4>Return Value:</h4>
                     <p>
                       <ul>
-                      <li><strong>0</strong>: If the points <code>p</code>, <code>q</code>, and <code>r</code> are collinear, the function would return 0 which implies that they lie on the same straight line.<br /></li>
-                      <li><strong>1</strong>: If the points <code>p</code>, <code>q</code>, and <code>r</code> form a clockwise orientation, the three points form a right turn.<br /></li>
-                      <li><strong>2</strong>: If the points <code>p</code>, <code>q</code>, and <code>r</code> form a counterclockwise orientation, i.e., they form a left turn.</li>
+                        <li>
+                          <strong>0</strong>: If the points <code>p</code>,{" "}
+                          <code>q</code>, and <code>r</code> are collinear, the
+                          function would return 0 which implies that they lie on
+                          the same straight line.
+                          <br />
+                        </li>
+                        <li>
+                          <strong>1</strong>: If the points <code>p</code>,{" "}
+                          <code>q</code>, and <code>r</code> form a clockwise
+                          orientation, the three points form a right turn.
+                          <br />
+                        </li>
+                        <li>
+                          <strong>2</strong>: If the points <code>p</code>,{" "}
+                          <code>q</code>, and <code>r</code> form a
+                          counterclockwise orientation, i.e., they form a left
+                          turn.
+                        </li>
                       </ul>
                     </p>
-                  
-                  <h4>Description:</h4>
-                  <p>
-                    The orientation of three points in a two-dimensional plane can be determined using the cross-product method.
-                    This method involves calculating the cross-product of two vectors formed by the line segments connecting the points. 
-                    <p>Consider two vectors:</p>
-                    <ul>
-                      <li>Vector v1 formed by the line segment from p to q.</li>
-                      <li>Vector v2 formed by the line segment from q to r.</li>
-                    </ul>
-                    
 
-                    
-                    <p>The cross-product of v1 and v2 can be computed as (v1_x * v2_y) - (v1_y * v2_x).</p>
-                      <p>The sign of the result of the cross-product determines the orientation:</p>
+                    <h4>Description:</h4>
+                    <p>
+                      The orientation of three points in a two-dimensional plane
+                      can be determined using the cross-product method. This
+                      method involves calculating the cross-product of two
+                      vectors formed by the line segments connecting the points.
+                      <p>Consider two vectors:</p>
                       <ul>
-                        <li>If the result is positive, it indicates a clockwise orientation, suggesting that the points p, q, and r form a right turn.</li>
-                        <li>If the result is negative, it indicates a counterclockwise orientation, suggesting that the points form a left turn.</li>
+                        <li>
+                          Vector v1 formed by the line segment from p to q.
+                        </li>
+                        <li>
+                          Vector v2 formed by the line segment from q to r.
+                        </li>
                       </ul>
-          
-                    
-                    
-                    
+                      <p>
+                        The cross-product of v1 and v2 can be computed as (v1_x
+                        * v2_y) - (v1_y * v2_x).
+                      </p>
+                      <p>
+                        The sign of the result of the cross-product determines
+                        the orientation:
+                      </p>
+                      <ul>
+                        <li>
+                          If the result is positive, it indicates a clockwise
+                          orientation, suggesting that the points p, q, and r
+                          form a right turn.
+                        </li>
+                        <li>
+                          If the result is negative, it indicates a
+                          counterclockwise orientation, suggesting that the
+                          points form a left turn.
+                        </li>
+                      </ul>
+                    </p>
                   </p>
-                  
-                 </p>
-               </div>
-             </div>
-           </div>
+                </div>
+              </div>
+            </div>
           )}
         </div>
         <div className="slideContent"></div>
